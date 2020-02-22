eff7d02fe21bf67eb6fd7ca15ef0e228e97bd82f
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-02-21 21:42:13 -0800

    destroy users and admin functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_053123) do
+ActiveRecord::Schema.define(version: 2020_02_22_051133) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_053123) do
     t.string "favorite_coffee"
     t.string "location"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
