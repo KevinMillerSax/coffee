807ea279a527406db1472d5efe2dbe44f75bc07c
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-02-28 23:22:25 -0800

    styling replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,15 +1,16 @@
 class RepliesController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @current_conversation = Conversation.find(reply_params[:conversation_id])
     @reply = @current_conversation.replies.new(reply_params)
     @reply.user = current_user
     if @reply.save
+      flash.alert = "bingo!"
       @reply.create_activity :create, owner: current_user
       redirect_to @current_conversation
     else
-      redirect_to root_url
+      flash.alert = "you must log in to reply!"
+      redirect_to @current_conversation
     end
   end
 
