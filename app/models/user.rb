815dd55d14eae9874f1250a77cbf4befbb6d8f3b
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-03-05 00:19:07 -0800

    styled the edit profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token
 
-  validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 2 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 2 },
+             allow_nil: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password
