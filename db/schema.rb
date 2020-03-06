85e4f282f85cec799db0891552a2ec965a4d2826
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-03-05 20:59:02 -0800

    database config

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_02_29_214930) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,13 +38,13 @@ ActiveRecord::Schema.define(version: 2020_02_29_214930) do
 
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
-    t.integer "trackable_id"
+    t.bigint "trackable_id"
     t.string "owner_type"
-    t.integer "owner_id"
+    t.bigint "owner_id"
     t.string "key"
     t.text "parameters"
     t.string "recipient_type"
-    t.integer "recipient_id"
+    t.bigint "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_214930) do
 
   create_table "conversations", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id", "created_at"], name: "index_conversations_on_user_id_and_created_at"
@@ -63,12 +66,12 @@ ActiveRecord::Schema.define(version: 2020_02_29_214930) do
 
   create_table "replies", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "conversation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"user_id\", \"conversation\", \"created_at\"", name: "index_replies_on_user_id_and_conversation_and_created_at"
     t.index ["conversation_id"], name: "index_replies_on_conversation_id"
+    t.index ["user_id", "conversation_id", "created_at"], name: "index_replies_on_user_id_and_conversation_id_and_created_at"
     t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
